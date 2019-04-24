3c6a74bda1a7b85da71452e87a72cae5880ec4f3
Srikanth Kamath <Srikanth@tskamath.com>
2019-04-24 13:46:35 +0530

    responsive styles with about-columns

--- templates/about.php
+++ templates/about.php
@@ -15,28 +15,28 @@
 
 get_header(); ?>
 	<div class="banner">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div id="primary" class="content-area">
-					<main id="main" class="site-main text-center" role="main">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div id="primary" class="content-area">
+						<main id="main" class="site-main text-center" role="main">
 
-						<?php
-						while ( have_posts() ) : the_post();
+							<?php
+							while ( have_posts() ) : the_post();
 
-							get_template_part( 'template-parts/content', 'page' );
+								get_template_part( 'template-parts/content', 'page' );
 
-							// If comments are open or we have at least one comment, load up the comment template.
-							if ( comments_open() || get_comments_number() ) :
-								comments_template();
-							endif;
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( comments_open() || get_comments_number() ) :
+									comments_template();
+								endif;
 
-						endwhile; // End of the loop.
-						?>
+							endwhile; // End of the loop.
+							?>
 
-					</main><!-- #main -->
-				</div><!-- #primary -->
-			</div><!-- .row -->
-		</div><!-- .container -->
+						</main><!-- #main -->
+					</div><!-- #primary -->
+				</div><!-- .row -->
+			</div><!-- .container -->
 	</div><!-- .banner -->
 	<?php
 		// Grab the metadata from the database
@@ -44,17 +44,18 @@ get_header(); ?>
 		$rightcol = get_post_meta( get_the_ID(), '_stanleywp_right', true );
 	?>
 
-
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<?php echo wp_kses_post($leftcol); ?>
-			</div><!-- .col-md-6 -->
-			<div class="col-md-6">
-				<?php echo wp_kses_post($rightcol); ?>
-			</div><!-- .col-md-6 -->
-		</div><!-- .row -->
-	</div><!-- .container -->
+	<div class="about-columns">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<?php echo wp_kses_post($leftcol); ?>
+				</div><!-- .col-md-6 -->
+				<div class="col-md-6">
+					<?php echo wp_kses_post($rightcol); ?>
+				</div><!-- .col-md-6 -->
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</div><!-- .about-columns -->
 <?php
 // get_sidebar();
 get_footer();
